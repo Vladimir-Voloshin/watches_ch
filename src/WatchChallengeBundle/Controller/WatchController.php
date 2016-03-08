121f1728601a0ee280aeeb43e79f42ca9a70ecab
Boris Doomer <doomer@localhost.localdomain>
2016-03-08 11:51:57 +0200

    before img update

--- src/WatchChallengeBundle/Controller/WatchController.php
+++ src/WatchChallengeBundle/Controller/WatchController.php
@@ -22,35 +22,46 @@ class WatchController extends Controller
     public function createAction(Request $request)
     {
         $data = $request->request->all();
+        $watch = new Watch();
 
         if (array_key_exists('brand_id', $data) && $data['brand_id']) {
             /** @var \WatchChallengeBundle\Repository\BrandRepository $brandRepo */
             $brandRepo = $this->getDoctrine()->getRepository('WatchChallengeBundle:Brand');
             $brand = $brandRepo->findOneBy(array('id' => $data['brand_id']));
+            $watch->setBrand($brand);
         }
 
         if (array_key_exists('case_material_id', $data) && $data['case_material_id']) {
             /** @var \WatchChallengeBundle\Repository\BrandRepository $caseMaterialRepo */
             $caseMaterialRepo = $this->getDoctrine()->getRepository('WatchChallengeBundle:CaseMaterial');
             $caseMaterial = $caseMaterialRepo->findOneBy(array('id' => $data['case_material_id']));
+            $watch->setCaseMaterial($caseMaterial);
         }
 
         if (array_key_exists('condition_id', $data) && $data['condition_id']) {
-            /** @var \WatchChallengeBundle\Repository\BrandRepository $watchConditionRepo */
+            /** @var \WatchChallengeBundle\Repository\WatchConditionRepository $watchConditionRepo */
             $watchConditionRepo = $this->getDoctrine()->getRepository('WatchChallengeBundle:WatchCondition');
             $watchCondition = $watchConditionRepo->findOneBy(array('id' => $data['condition_id']));
+            $watch->setCondition($watchCondition);
         }
 
-        $watch = new Watch();
+        if (array_key_exists('watch_functions', $data) && $data['watch_functions'] && is_array($data['watch_functions'])) {
+            /** @var \WatchChallengeBundle\Repository\WatchFunctionRepository $watchFunctionRepo */
+            $watchFunctionRepo = $this->getDoctrine()->getRepository('WatchChallengeBundle:WatchFunction');
+            foreach($data['watch_functions'] as $functionId){
+                $function = $watchFunctionRepo->findOneBy(array('id' => $functionId));
+                if(!empty($function)){
+                    $watchFunctions[] = $function;
+                }
+            }
+            $watch->setWatchFunctions($watchFunctions);
+        }
+
         $watch->setModel($data['model']);
         $watch->setCaseSite($data['case_site']);
         $watch->setSku($data['sku']);
         $watch->setBracelet($data['bracelet']);
         $watch->setSlug($data['model'] . '-' .$data['sku']);
-
-        $watch->setBrand($brand);
-        $watch->setCondition($watchCondition);
-        $watch->setCaseMaterial($caseMaterial);
 
         $em = $this->getDoctrine()->getEntityManager();
         $em->persist($watch);
@@ -66,10 +77,104 @@ class WatchController extends Controller
             $image->setPath($image::BaseUrl . $uniqid . $file->getClientOriginalName());
 
             $file->move($image::BaseUrl, $uniqid . $file->getClientOriginalName());
+            $em->persist($image);
             $em->persist($watch);
             $em->flush();
         }
 
+
+        return new JsonResponse(array(
+            'watch' => $watch->toJson()
+        ));
+    }
+
+    public function deleteAction($sku)
+    {
+        /** @var \WatchChallengeBundle\Repository\WatchRepository $watchRepository */
+        $watchRepository = $this->getDoctrine()->getRepository('WatchChallengeBundle:Watch');
+        $item = $watchRepository->findBy(array('sku' => $sku));
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->remove($item);
+        $em->flush();
+        return new JsonResponse(array(
+            'request' => 'OK'
+        ));
+    }
+
+    public function updateAction(Request $request)
+    {
+        $data = $request->request->all();
+
+        /** @var \WatchChallengeBundle\Repository\WatchRepository $watchRepository */
+        $watchRepository = $this->getDoctrine()->getRepository('WatchChallengeBundle:Watch');
+        $watch = $watchRepository->findOneBy(array('id' => $data['watch_id']));
+
+
+        if (array_key_exists('brand_id', $data) && $data['brand_id']) {
+            /** @var \WatchChallengeBundle\Repository\BrandRepository $brandRepo */
+            $brandRepo = $this->getDoctrine()->getRepository('WatchChallengeBundle:Brand');
+            $brand = $brandRepo->findOneBy(array('id' => $data['brand_id']));
+            $watch->setBrand($brand);
+        }
+
+        if (array_key_exists('case_material_id', $data) && $data['case_material_id']) {
+            /** @var \WatchChallengeBundle\Repository\BrandRepository $caseMaterialRepo */
+            $caseMaterialRepo = $this->getDoctrine()->getRepository('WatchChallengeBundle:CaseMaterial');
+            $caseMaterial = $caseMaterialRepo->findOneBy(array('id' => $data['case_material_id']));
+            $watch->setCaseMaterial($caseMaterial);
+        }
+
+        if (array_key_exists('condition_id', $data) && $data['condition_id']) {
+            /** @var \WatchChallengeBundle\Repository\WatchConditionRepository $watchConditionRepo */
+            $watchConditionRepo = $this->getDoctrine()->getRepository('WatchChallengeBundle:WatchCondition');
+            $watchCondition = $watchConditionRepo->findOneBy(array('id' => $data['condition_id']));
+            $watch->setCondition($watchCondition);
+        }
+
+        if (array_key_exists('watch_functions', $data) && $data['watch_functions'] && is_array($data['watch_functions'])) {
+            /** @var \WatchChallengeBundle\Repository\WatchFunctionRepository $watchFunctionRepo */
+            $watchFunctionRepo = $this->getDoctrine()->getRepository('WatchChallengeBundle:WatchFunction');
+            foreach($data['watch_functions'] as $functionId){
+                $function = $watchFunctionRepo->findOneBy(array('id' => $functionId));
+                if(!empty($function)){
+                    $watchFunctions[] = $function;
+                }
+            }
+            $watch->setWatchFunctions($watchFunctions);
+        }
+
+        if (array_key_exists('model', $data) && $data['model']) {
+            $watch->setModel($data['model']);
+        }
+        if (array_key_exists('case_site', $data) && $data['case_site']) {
+            $watch->setModel($data['case_site']);
+        }
+        if (array_key_exists('sku', $data) && $data['sku']) {
+            $watch->setModel($data['sku']);
+        }
+        if (array_key_exists('bracelet', $data) && $data['bracelet']) {
+            $watch->setModel($data['bracelet']);
+        }
+
+
+        $em = $this->getDoctrine()->getEntityManager();
+        foreach($request->files as $file){
+            if(null === $file && in_array($file->getClientMimeType(), array('image/jpeg'))){
+                continue;
+            }
+            $image = new WatchImage();
+            $uniqid = uniqid('', true);
+            $watch->addWatchImages($image);
+            $image->setPath($image::BaseUrl . $uniqid . $file->getClientOriginalName());
+
+            $file->move($image::BaseUrl, $uniqid . $file->getClientOriginalName());
+            $em->persist($image);
+            $em->persist($watch);
+            $em->flush();
+        }
+
+        $em->persist($watch);
+        $em->flush();
 
         return new JsonResponse(array(
             'watch' => $watch->toJson()
